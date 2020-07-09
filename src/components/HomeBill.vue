84dee595467fba479c41018cef0b91078a8f2553
Kirill <qSatan1s@yandex.ru>
2020-07-09 10:29:12 +0300

    first commit

---
+++ src/components/HomeBill.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="col s12 m6 l4">
+    <div class="card light-blue bill-card">
+      <div class="card-content white-text">
+        <span class="card-title">{{'BillInCurrency'|localize}}</span>
+
+        <p v-for="cur of currencies" :key="cur" class="currency-line">
+          <span>{{ getCurrency(cur) | currency(cur) }}</span>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['rates'],
+  data: () => ({
+    currencies: ['UAH', 'USD', 'EUR']
+  }),
+  computed: {
+    base() {
+      return (
+        this.$store.getters.info.bill /
+        (this.rates['UAH'].Value / this.rates['USD'].Value)
+      )
+    }
+  },
+  methods: {
+    getCurrency(currency) {
+      if (currency === this.currencies[0]) {
+        return this.$store.getters.info.bill
+      }
+      return Math.floor(this.base / this.rates[currency].Value)
+    }
+  }
+}
+</script>
